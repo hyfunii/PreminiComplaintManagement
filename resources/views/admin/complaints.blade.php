f51c1f78a8ba5b0b8e48373f35c6f19b18d74f14
Nama Siswa <email_siswa@example.com>
2024-10-22 08:12:44 +0700

    p

--- resources/views/admin/complaints.blade.php
+++ resources/views/admin/complaints.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
+
+
 <!-- Complaints Table -->
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
